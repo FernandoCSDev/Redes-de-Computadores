6267c9008e5fbaf055a9424e082b5f626de20da0
Fernando Carneiro <fcarneiro2018@gmail.com>
2024-02-20 10:23:32 -0400

    uptade

--- src/Teste.java
+++ src/Teste.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Teste {
     public static void main(String[] args) {
+
+
         Scanner leitor = new Scanner(System.in);
         String teste;
         teste = leitor.next();
@@ -17,6 +19,25 @@ public class Teste {
 
         System.out.println(num2);
 
+//      172.30.1.33
+//      255.255.0.0
 
+//      172.30.1.33
+//      255.255.255.0
+
+//      192.168.10.234
+//      255.255.255.0
+//
+//      172.17.99.71
+//      255.255.0.0
+//
+//      192.168.3.219
+//      255.255.0.0
+
+//      172.25.114.250
+//      255.255.0.0
+
+//    192.168.3.219
+//    255.255.255.224
     }
 }
